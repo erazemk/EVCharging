dcf370218e8129027a1cc3756a58e7c94b8e1337
Ana Strmcnik <strmcnik.ana@gmail.com>
2021-10-30 12:02:52 +0200

    Add Entity

---
+++ entitete/src/main/java/Entity.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public class Entity implements Serializable {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
