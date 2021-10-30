96e4ef8b128dddd02e7cddb543c1fe290f62f0a9
Ana Strmcnik <strmcnik.ana@gmail.com>
2021-10-30 12:24:00 +0200

    Add class Owner, Person, User

---
+++ entitete/src/main/java/Owner.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Owner extends Person {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private ArrayList<String> ownedStations; //TODO: Fix when station class implemented
+
+    //TODO: Generate getters and setters
+}
---
+++ entitete/src/main/java/User.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class User extends Person {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private ArrayList<String> charges; //TODO: Fix when charge class implemented
+    private ArrayList<String> reservations; //TODO: Fix when reservation class implemented
+
+    //TODO: Generate getters and setters
+}
