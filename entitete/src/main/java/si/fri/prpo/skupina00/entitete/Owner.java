081ac613333b00e534a610333efc0ed961bbf90e
Ana Strmcnik <strmcnik.ana@gmail.com>
2021-11-01 10:29:29 +0100

    Implement JPA on Owner

--- entitete/src/main/java/si/fri/prpo/skupina00/entitete/Owner.java
+++ entitete/src/main/java/si/fri/prpo/skupina00/entitete/Owner.java
@@ -1,12 +1,28 @@
 package si.fri.prpo.skupina00.entitete;
 
+import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.ArrayList;
 
+@Entity(name = "owner")
+@NamedQueries(value = {
+        @NamedQuery(name = "Owner.getAll", query = "SELECT o FROM owner o"),
+        @NamedQuery(name = "Owner.allStations", query = "SELECT s.stationName FROM owner o, station s WHERE o.email = :email"),
+        @NamedQuery(name = "owner.deleteOwnerViaEmail", query = "DELETE FROM owner WHERE email=:email"),
+        @NamedQuery(name = "owner.updateOwnerEmailViaEmail", query = "UPDATE owner SET email =:newEmail WHERE email=:oldEmail")
+})
+
 public class Owner extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String surname;
     private String email;
+
+    @OneToMany
+    @JoinColumn(name = "ownerId")
     private ArrayList<Station> ownedStations;
 
     public ArrayList<Station> getOwnedStations() {
