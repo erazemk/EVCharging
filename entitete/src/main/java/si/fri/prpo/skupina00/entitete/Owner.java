d518037b6ff9260d12628fff8cf2bbd291319526
Erazem Kokot <mail@erazem.eu>
2021-11-14 11:02:16 +0100

    Temporarily re-add methods from Person to User and Owner

--- entitete/src/main/java/si/fri/prpo/skupina00/entitete/Owner.java
+++ entitete/src/main/java/si/fri/prpo/skupina00/entitete/Owner.java
@@ -33,6 +33,30 @@ public class Owner extends Person {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public List<Station> getOwnedStations() {
         return ownedStations;
     }
--- entitete/src/main/java/si/fri/prpo/skupina00/entitete/User.java
+++ entitete/src/main/java/si/fri/prpo/skupina00/entitete/User.java
@@ -36,6 +36,30 @@ public class User extends Person {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public List<Charge> getCharges() {
         return charges;
     }
