5aef4fba0dd79705fe9ec918c075aa8ba55a0588
Erazem Kokot <mail@erazem.eu>
2021-11-15 20:32:25 +0100

    Specify serial version UID for Entity

--- entities/src/main/java/si/fri/prpo/skupina00/evcharging/entities/Entity.java
+++ entities/src/main/java/si/fri/prpo/skupina00/evcharging/entities/Entity.java
@@ -3,6 +3,8 @@ package si.fri.prpo.skupina00.evcharging.entities;
 import java.io.Serializable;
 
 public abstract class Entity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
 
     public Integer getId() {
