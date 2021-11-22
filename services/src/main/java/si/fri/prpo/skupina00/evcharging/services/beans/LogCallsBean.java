e6537ad8ae3d6259ee0269c287dbcf4e1a310536
Ana Strmcnik <strmcnik.ana@gmail.com>
2021-11-22 14:16:17 +0100

    Implement LogCallsBean.java

--- services/src/main/java/si/fri/prpo/skupina00/evcharging/services/beans/LogCallsBean.java
+++ services/src/main/java/si/fri/prpo/skupina00/evcharging/services/beans/LogCallsBean.java
@@ -1,4 +1,24 @@
 package si.fri.prpo.skupina00.evcharging.services.beans;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@ApplicationScoped
 public class LogCallsBean {
+
+    private static final Logger log = Logger.getLogger(LogCallsBean.class.getName());
+    private Map<Integer, Integer> mapMethodCounter = new HashMap<>();
+
+    public void methodCallCounter(Integer hashCodeMethod, String name) {
+        if(!mapMethodCounter.containsKey(hashCodeMethod)) {  //  Method not yet in hashMap
+            mapMethodCounter.put(hashCodeMethod, 1);
+        } else {  //  Method is already in hashMap
+            mapMethodCounter.replace(hashCodeMethod, mapMethodCounter.get(hashCodeMethod)+1);
+        }
+
+        log.info("Metoda " + name + " je bila poklicana: " + mapMethodCounter.get(hashCodeMethod) + "-krat.");
+
+    }
 }
