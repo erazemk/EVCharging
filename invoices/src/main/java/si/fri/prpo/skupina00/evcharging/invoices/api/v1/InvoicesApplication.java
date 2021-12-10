2d993862352af31dde96d8cd11ae640ac1bf9464
Ana Strmčnik <strmcnik.ana@gmail.com>
2021-12-10 09:26:33 +0100

    Add structure for invoices microservice

---
+++ invoices/src/main/java/si/fri/prpo/skupina00/evcharging/invoices/api/v1/InvoicesApplication.java
@@ -0,0 +1,4 @@
+package si.fri.prpo.skupina00.evcharging.invoices.api.v1;
+
+public class InvoicesApplication {
+}
---
+++ invoices/src/main/java/si/fri/prpo/skupina00/evcharging/invoices/api/v1/resources/InvoicesResource.java
@@ -0,0 +1,4 @@
+package si.fri.prpo.skupina00.evcharging.invoices.api.v1.resources;
+
+public class InvoicesResource {
+}
